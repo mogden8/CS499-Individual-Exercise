c4cee08407e89f993120381762453a0c496f3f5f
mogden8 <71397057+mogden8@users.noreply.github.com>
2021-09-16 14:55:09 -0700

    Added printArray() and sortPrintArray() (Letters Only)
    Added a print function and a sort/print function that sorts the letters from the array. Added method calls to main to run sort function.

--- GitExercise/src/GitExercise/sortArray.java
+++ GitExercise/src/GitExercise/sortArray.java
@@ -6,7 +6,12 @@ public class sortArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//create random array
+		ArrayList array = createArray();
+		//print random array
+		printArray(array);
+		//sort letters from array and print
+		sortPrintArray(array);
 	}
 	
 public static ArrayList createArray() {
@@ -20,5 +25,32 @@ public static ArrayList createArray() {
 		}
 		return Array;
 	}
+
+public static void printArray(ArrayList A) {
+	int y=0;
+	for (int x=A.size()-1;x>0; x--) {
+		System.out.println(A.get(x));
+	}
+}
+
+public static void sortPrintArray(ArrayList A) {
+	
+	ArrayList<Character> letterArray= new ArrayList<Character>();
+	
+	for (int x=A.size()-1;x>0; x--) {
+		char y=(char)A.get(x);
+		if (Character.isLetter(y))
+			 letterArray.add(y);
+	}
+	
+	System.out.println("Letters: ");
+	
+	for (int x=letterArray.size()-1;x>0; x--) {
+		System.out.print(letterArray.get(x));
+	}
+	
+	
+}
+
 
 }
