5614db8eb8300ccd570801d6cff19d64f76eaf00
mogden8 <71397057+mogden8@users.noreply.github.com>
2021-09-16 15:19:14 -0700

    Added Testing if Letters are Sorted

--- GitExercise/src/GitExercise/sortArrayTest.java
+++ GitExercise/src/GitExercise/sortArrayTest.java
@@ -11,11 +11,17 @@ class sortArrayTest {
 
 	@Test
 	void test() {
-		//fail("Not yet implemented");
+		//testing if array is empty
 		ArrayList<Character> A= sortArray.createArray();
 		Boolean arrayEmpty= A.isEmpty();
 		assertFalse(arrayEmpty);
-	
+		
+		ArrayList<Character> Letters = sortArray.sortArrayLetters(A);
+		
+		for (int x=Letters.size()-1;x>0; x--) {
+			assertTrue(Character.isLetter(Letters.get(x)));
+		}
+		
 		
 	}
 
