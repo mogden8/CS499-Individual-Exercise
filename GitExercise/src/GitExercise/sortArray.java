ef510c2597d1af75627a545d1d4b148daf0d2432
mogden8 <71397057+mogden8@users.noreply.github.com>
2021-09-16 14:49:13 -0700

    Added createArray() Function

--- GitExercise/src/GitExercise/sortArray.java
+++ GitExercise/src/GitExercise/sortArray.java
@@ -1,10 +1,24 @@
 package GitExercise;
 
+import java.util.*;
+
 public class sortArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+public static ArrayList createArray() {
+		
+		ArrayList<Character> Array= new ArrayList<Character>();
+		Random r = new Random();
+		
+		for (int x=0; x<=100; x++) {
+			char c = (char)(r.nextInt(26) + '0');
+			Array.add(c);
+		}
+		return Array;
 	}
 
 }
